58c93c47bf5dbec6702c924a65bcdf8a68df1679
JimmyChenkai123 <chenkaiscu@163.com>
2021-09-09 17:14:35 +0800

    解决同一时间无法触发调度BUG

--- src/main/java/com/ppio/bd/ksdata/KsDataToOssApplication.java
+++ src/main/java/com/ppio/bd/ksdata/KsDataToOssApplication.java
@@ -3,17 +3,19 @@ package com.ppio.bd.ksdata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 
 /**
- *  测试环境
- *  * nohup java -jar -Dspring.profiles.active=dev  ks_data_to_oss-0.0.1-SNAPSHOT.jar > /dev/null 2>&1  &
- *  *
- *  * 生产环境
- *
- *
- *  部署在机器   172.16.76.185   ,   目录  /opt/ksDataToOss
+ * 测试环境
+ * * nohup java -jar -Dspring.profiles.active=dev  ks_data_to_oss-0.0.1-SNAPSHOT.jar > /dev/null 2>&1  &
+ * *
+ * * 生产环境
+ * <p>
+ * <p>
+ * 部署在机器   172.16.76.185   ,   目录  /opt/ksDataToOss
  */
 @EnableScheduling
 @SpringBootApplication
@@ -23,6 +25,16 @@ public class KsDataToOssApplication {
         SpringApplication.run(KsDataToOssApplication.class, args);
     }
 
+    /**
+     *  解决springboot 无法统一时间调度触发任务
+     * @return
+     */
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskExecutor = new ThreadPoolTaskScheduler();
+        taskExecutor.setPoolSize(50);
+        return taskExecutor;
+    }
 
 
 }
